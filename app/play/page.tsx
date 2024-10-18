692179e6f06f0c543ba7707a8d171d1c48825708
Alexey Andreev <a.andreev@passwork.team>
2024-10-18 04:58:03 +0300

    nextjs

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,10 +1,8 @@
 import {promises as fs} from "fs";
 import {Play} from "../ui/play";
 
-export default async function Page({searchParams}: {
-    searchParams: { [key: string]: string | string[] | undefined }
-}) {
-    const play = searchParams.name;
+export default async function Page() {
+    const play = "millhell";
     const regex = new RegExp('^[a-z]+$');
     if (typeof play !== "string" || !regex.test(play)) {
         return  <div>
