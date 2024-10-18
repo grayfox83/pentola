02d2c87a2cd24f0a66499faadde452aed7d216a5
Alexey Andreev <a.andreev@passwork.team>
2024-10-18 06:24:24 +0300

    nextjs

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,32 +3,41 @@ import {Play} from "../ui/play";
 import {useSearchParams} from "next/navigation";
 import {useEffect, useState, Suspense} from "react";
 
-export default function Page() {
+function GetData() {
     const [data, setData] = useState(null)
     const [isLoading, setLoading] = useState(true)
+    const [isHydrated, setIsHydrated] = useState(false);
     const searchParams = useSearchParams();
     let play = searchParams.get('name')
     const regex = new RegExp('^[a-z]+$');
     if (typeof play !== "string" || !regex.test(play)) {
-       play = "error";
+        play = "error"; //TODO
     }
     useEffect(() => {
-        fetch('/plays/' + play + '.json').then(r => r.json()).then(data => {
-                setData(data)
+        setIsHydrated(true);
+        if (isHydrated) { //loading once
+            fetch('/plays/' + play + '.json').then(r => r.json()).then(data => {
+                    setData(data)
+                    setLoading(false)
+                }
+            ).catch(() => {
+                setData(null)
                 setLoading(false)
-            }
-        ).catch(() => {
-            setData(null)
-            setLoading(false)
-        })
-    }, ['play']);
+            })
+        }
+    }, [play, isHydrated]);
     if (isLoading) return <div>Загрузка...</div>
     if (!data) return <div>Нет данных</div>
+    return <div>
+             <a className={'font-medium text-blue-600 dark:text-blue-500'} href={'/'}>НАЗАД</a>
+            <Play play={data}/>
+        </div>
+}
+
+export default function Page() {
+
 
     return <Suspense fallback={<div>Загрузка...</div>}>
-        <div>
-        <a className={'font-medium text-blue-600 dark:text-blue-500'} href={'/'}>НАЗАД</a>
-        <Play play={data}/>
-        </div>
+        <GetData></GetData>
     </Suspense>;
 }
