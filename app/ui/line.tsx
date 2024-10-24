aa263018ba6cfa1a9edda00480fbe3a15e6628b2
Alexey Andreev <a.andreev@passwork.team>
2024-10-24 13:19:40 +0300

    nextjs

--- app/ui/line.tsx
+++ app/ui/line.tsx
@@ -12,10 +12,10 @@ export function Line({
 }) {
     return (
         <div className='play-line flex flex-row w-full p-2'>
-            <div className={'w-64'} role-id={roleId}>
+            <div className={'w-32'} role-id={roleId}>
                 <span>{role}</span>
             </div>
-            <div className={'flex flex-col basis-4/5'} role-id={roleId}>
+            <div className={'flex flex-col'} role-id={roleId}>
                 {line.filter(text => text.s).map( (text, index) => {
                     return <span key={index} className={'play-line-text ' + (text.action? 'italic ': ' ')}>{text.s}<br/></span>
                 })}
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/pentola'
-};
+import constants from 'next/constants.js';
 
-export default nextConfig;
+export default (phase, { defaultConfig }) => {
+    /** @type {import('next').NextConfig} */
+    const devNextConfig = {
+    };
+
+    /** @type {import('next').NextConfig} */
+    const prodNextConfig = {
+        basePath: "/pentola"
+    };
+
+    if (phase === constants.PHASE_DEVELOPMENT_SERVER) {
+        return devNextConfig;
+    }
+    return prodNextConfig;
+}
