0f9e899d4d5edb275fb0c1bc663c85d3ba4a9daa
Alexey Andreev <a.andreev@passwork.team>
2024-10-24 13:36:17 +0300

    nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
-import constants from 'next/constants.js';
+// import constants from 'next/constants.js';
+//
+// export default (phase, { defaultConfig }) => {
+//     /** @type {import('next').NextConfig} */
+//     const devNextConfig = {
+//     };
+//
+//     /** @type {import('next').NextConfig} */
+//     const prodNextConfig = {
+//         basePath: "/pentola"
+//     };
+//
+//     if (phase === constants.PHASE_DEVELOPMENT_SERVER) {
+//         return devNextConfig;
+//     }
+//     return prodNextConfig;
+// }
 
-export default (phase, { defaultConfig }) => {
-    /** @type {import('next').NextConfig} */
-    const devNextConfig = {
-    };
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-    /** @type {import('next').NextConfig} */
-    const prodNextConfig = {
-        basePath: "/pentola"
-    };
-
-    if (phase === constants.PHASE_DEVELOPMENT_SERVER) {
-        return devNextConfig;
-    }
-    return prodNextConfig;
-}
+export default nextConfig;
