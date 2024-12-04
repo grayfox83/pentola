656ba5f942037d0843acf448cdee7290ac2e8e6a
Alexey Andreev <a.andreev@passwork.team>
2024-12-04 23:37:48 +0300

    css

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     return <div className={'flex flex-col items-center h-max'}>
         <h1 className={'text-2xl mb-8'}>Пьесы:</h1>
         {library.plays?.map( (play) =>
-        <a className={'font-medium text-blue-600 dark:text-blue-500'} href={'play?name='+play.filename} key={play.id}>{play.name}</a>
+        <a className={'play font-medium text-blue-600 dark:text-blue-500'} href={'play?name='+play.filename} key={play.id}>{play.name}</a>
         )}
     </div>
 }
